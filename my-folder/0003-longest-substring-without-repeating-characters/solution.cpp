805a25079f454be117cdd20728ea3845ec55182e
psurge1 <suraj.sswamy@gmail.com>
2024-09-16 03:28:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.2 MB (99.97%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        set<char> lset;
-        int maxlength = 0;
-        for (int i = 0, k, n = s.length(); i < n; ++i)
-        {
-            lset.clear();
-            k = 0;
-            for (int j = i; j < n; ++j)
-            {
-                if (lset.find(s[j]) == lset.end())
-                {
-                    lset.insert(s[j]);
-                    ++k;
+        int stringSize = s.size();
+        if (stringSize < 2) {
+            return stringSize;
+        }
+
+        int maxLength = 1;
+        int leftPtr = 0;
+        int rightPtr = 1;
+        int indexOfCharacters[95]; // hash function: character - 32
+        for (int arrIndex = 0; arrIndex < 95; ++arrIndex) {
+            indexOfCharacters[arrIndex] = -1;
+        }
+
+        indexOfCharacters[s[leftPtr] - 32] = 0;
+        while (rightPtr < stringSize) {
+            char currentChar = s[rightPtr];
+            if (indexOfCharacters[currentChar - 32] == -1) {
+                indexOfCharacters[currentChar - 32] = rightPtr;
+                ++rightPtr;
+                maxLength = rightPtr - leftPtr > maxLength ? rightPtr - leftPtr : maxLength;
+            }
+            else {
+                int temp = leftPtr;
+                leftPtr = indexOfCharacters[currentChar - 32] + 1;
+                for (temp; temp < leftPtr; ++temp) {
+                    indexOfCharacters[s[temp] - 32] = -1;
                 }
-                else
-                {
-                    maxlength = k > maxlength ? k : maxlength;
-                    j = s.length();
-                }
-                if (j == n - 1)
-                {
-                    maxlength = k > maxlength ? k : maxlength;
-                    j = s.length();
-                }
+                indexOfCharacters[currentChar - 32] = rightPtr;
+                ++rightPtr;
             }
         }
-        return maxlength;
+
+        return maxLength;
     }
 };
