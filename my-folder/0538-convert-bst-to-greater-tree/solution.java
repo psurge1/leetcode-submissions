108a1100643a2f96d03916aa2423d4e5aeba14f2
psurge1 <suraj.sswamy@gmail.com>
2025-06-05 22:32:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.9 MB (100.00%)

---
+++ my-folder/0538-convert-bst-to-greater-tree/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Integer runningSum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null)
+            return null;
+        
+        dfs(root);
+
+        return root;
+    }
+
+    public void dfs(TreeNode node) {
+        if (node == null)
+            return;
+        
+        dfs(node.right);
+        node.val += runningSum;
+        runningSum = node.val;
+        dfs(node.left);
+    }
+}
